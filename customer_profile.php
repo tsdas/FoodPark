4a0891de0bc4967209953a376278e2cd088df321
zyandeep <zyandeep000@gmail.com>
2018-05-01 13:48:00 +0530

    working on customer profile

--- customer_profile.php
+++ customer_profile.php
@@ -1,14 +1,62 @@
-<?php 
+<?php session_start(); ?>
 
-session_start();
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-echo "<h1> Welcome home, $_SESSION[c_name] </h1>";
-echo "<hr>";
-?>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+    <div class="container">
+      
+    <?php 
+        require 'include/header.php';
+    ?> 
+
+    <div class="container" style="margin-top: 20px;">
+      <div class="row">
+        <div class="col-md-3"></div>
+        
+        <div class="col-md-3">
+            <div class="card">
+               <img class="card-img-top" src="img/update_user.jpg" alt="Card image" style="width:100%">
+               <div class="card-body">
+                 <a href="#" class="btn btn-primary">Update Profile</a>
+               </div>
+             </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+               <img class="card-img-top" src="img/list.png" alt="Card image" style="width:100%">
+               <div class="card-body">
+                 <a href="#" class="btn btn-primary">View Orders</a>
+               </div>
+             </div>
+        </div>
+
+        <div class="col-md-3"></div>
+      </div>
+    </div>
 
 
-<p> Click below to logout </p>
+    <?php 
+        require 'include/footer.php';
+    ?> 
 
-<form method="POST" action="customer_auth.php">
-	<input type="submit" name="logout" value="Log out">
-</form>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
